d0c61851df1a693dec633e7a6a6b4760d426d26a
IxAntho <isabar735@icloud.com>
2024-03-17 21:25:38 -0400

    Refactor js and scss

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,23 +1,28 @@
 //constants
+// Render images
 const urls = [
   {
     link: "https://images.unsplash.com/photo-1606503809729-40646b716a36?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8R3VhdGVtYWxhfGVufDB8fDB8fHww",
+    alt: "Atitlan Lake photo",
   },
   {
     link: "https://images.unsplash.com/photo-1528543010705-e7e75169b717?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8R3VhdGVtYWxhfGVufDB8fDB8fHww",
+    alt: "Atitlan lake photo from a dock",
   },
   {
     link: "https://images.unsplash.com/photo-1508035460735-91088c495500?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fEd1YXRlbWFsYXxlbnwwfHwwfHx8MA%3D%3D",
+    alt: "Tikal, Petén photo",
   },
   {
     link: "https://images.unsplash.com/photo-1584908917822-6799d69a72cc?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NzF8fEd1YXRlbWFsYXxlbnwwfHwwfHx8MA%3D%3D",
+    alt: "Antigua Guatemala photo",
   },
 ];
 const container = document.querySelector(
   ".container-lg.themed-container.text-center"
 );
 const rowTemplate = document.querySelector("#rowTemplate");
-const images = document.querySelectorAll(".col-md-3.themed-grid-col");
+// View mode
 const expandIcons = document.querySelectorAll(".fa-solid.fa-expand");
 
 // Render 20 images
@@ -31,19 +36,7 @@ for (let i = 0; i < 5; i++) {
     if (index < urls.length) {
       const imgUrl = urls[index].link;
       label.style.backgroundImage = `url(${imgUrl})`;
-
-      //   // Add click event listener to the label
-      //   label.addEventListener("click", function () {
-      //     const checkbox = this.querySelector(".image-checkbox");
-      //     this.classList.toggle("checked");
-      //     checkbox.checked = !checkbox.checked;
-      //   });
-
-      //   // Add click event listener to the checkbox
-      //   const checkbox = label.querySelector(".image-checkbox");
-      //   checkbox.addEventListener("click", function (event) {
-      //     event.stopPropagation();
-      //   });
+      label.setAttribute("aria-label", urls[index].alt);
     }
   });
 
@@ -60,13 +53,11 @@ document.addEventListener("DOMContentLoaded", function () {
 
   expandIcons.forEach((icon) => {
     icon.addEventListener("click", function (evt) {
-      console.log("click expand");
       const parentColumn = evt.target.closest(".col-md-3.themed-grid-col");
       const imageLabel = parentColumn.querySelector(".image-label");
       const clickedImageSrc = imageLabel.style.backgroundImage
         .slice(4, -1)
         .replace(/"/g, "");
-      console.log(clickedImageSrc);
 
       imageViewImage.src = clickedImageSrc;
       imageViewSection.classList.toggle("image-view_active");
